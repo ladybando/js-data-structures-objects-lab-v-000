3088ecbb342eb8b87db157f4bdc4e03ca4f8988e
Ayana Bando <ayanaflax@yahoo.com>
2018-11-27 16:46:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 }
 
 function deleteFromDriverByKey(driver, key){
-  Object.assign({}, driver, key); 
+  return Object.assign({}, driver, key); 
 }
