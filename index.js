871f6fb78ef8361020b95e98e8f1e7060ff424ae
Ayana Bando <ayanaflax@yahoo.com>
2018-11-27 16:34:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  
+    driver[key] = value;
+ 
+  return driver;
 }
